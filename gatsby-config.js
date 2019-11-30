1edbc5ef4f8049d6bee0a723241d5f2effc2a1bc
milecki <m.kulecki@gmail.com>
2019-11-30 23:54:40 +0100

    Adds plugins: manifest (with favicon) and offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,20 +16,18 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `kulecki website`,
+        short_name: `kulecki`,
+        start_url: `/`,
+        background_color: `#55198c`,
+        theme_color: `#55198c`,
+        display: `minimal-ui`,
+        icon: `src/images/favicon.svg`,
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
 }
