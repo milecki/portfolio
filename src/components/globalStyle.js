9afb50c851068b2e4fc16459b5ff8264f46c0434
milecki <m.kulecki@gmail.com>
2020-12-06 21:18:32 +0100

    fix: flash of unstyled text when switching language

--- src/components/globalStyle.js
+++ src/components/globalStyle.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components"
 
 const GlobalStyle = createGlobalStyle`
 
-@import url("https://fonts.googleapis.com/css?family=Josefin+Sans&display=swap");
+@import url("https://fonts.googleapis.com/css?family=Josefin+Sans&display=block");
 
 html,
 body,
